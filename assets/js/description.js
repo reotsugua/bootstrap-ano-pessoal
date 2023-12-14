577d8c1076fca095f15b474313d314eed0f836ac
Renan Santos <renan.santos@grupoeuro17.com.br>
2023-12-14 16:20:57 -0300

    feat: formulario + modal funcional

---
+++ assets/js/description.js
@@ -0,0 +1,18 @@
+const descriptionYear1 = `Ano pessoal 1 tem muita sorte. Isso porque esse número representa recomeços. Sendo assim, é o momento perfeito para dar o pontapé inicial em seus objetivos, já que tudo conspira para que eles deem certo!
+Durante esse período, é provável que você se sinta bastante independente. O seu espírito de liderança vai estar a mil, o que influencia ainda mais os seus projetos, sejam eles pessoais ou profissionais; tudo isso de forma positiva, é claro.
+Se você vem sendo cauteloso, agora é o momento para deixar a sua ambição correr solta e partir em uma jornada de autoconhecimento. Valorize a sua própria opinião e intuição, pois você está sendo influenciado por energias positivas. Tenha fé em você mesmo, pois tudo conspira a seu favor!
+`;
+const descriptionYear2 = "Ano pessoal 2 pode ser muito devagar... [Texto completo]";
+const descriptionYear3 =
+    "Ano pessoal 3 também influencia bastante a nossa expressão... [Texto completo]";
+const descriptionYear4 = "Aqui vai o texto do ano pessoal 4";
+const descriptionYear5 = "Aqui vai o texto do ano pessoal 5";
+const descriptionYear6 = "Aqui vai o texto do ano pessoal 6";
+const descriptionYear7 = "Aqui vai o texto do ano pessoal 7";
+const descriptionYear8 = "Aqui vai o texto do ano pessoal 8";
+const descriptionYear9 = `Ano pessoal 9 é natural que ele faça referência ao fim de uma era.
+Sendo assim, é comum que as pessoas regidas por esse número se encontrem em várias situações que representam a finalização de um ciclo.Mas calma! Não precisa se desesperar. Isso não quer dizer que tudo de bom ou de ruim simplesmente irá desaparecer de sua vida, mas que esses elementos irão se renovar e se reapresentar de maneira um pouco diferenciada no próximo ciclo que se inicia! Dessa maneira, tudo aquilo que acontece em sua trajetória acaba encontrando um equilíbrio maior.
+Essas transformações podem fazer com que tudo pareça estar um pouco mais agitado. Assim, é comum que o indivíduo regido por esse número se sinta um pouco impaciente e até mesmo angustiado, mas é importante manter a calma e olhar para esse momento com orgulho, afinal de contas é mais uma era com sucesso em sua história!
+É interessante também o fato de que é muito comum que as pessoas acabem se livrando de tudo o que é ruim em suas vidas durante esse período. Podemos dizer, então, que elas se preparam para iniciar um novo ciclo de forma mais positiva e mais otimista.
+É necessário, contudo, ficar atento para não acabar se excedendo e entrando em alguma situação que lhe coloque em problemas. Pense com muita seriedade em todas as medidas que você irá tomar e as consequências que elas irão lhe trazer.
+`;
